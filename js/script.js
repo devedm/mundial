bad9090694b91577394d29c22af66ad9a8ec6843
Eddy Mena <developer.eddymena@gmail.com>
2022-07-12 23:24:18 -0600

    added accordion for each contry

--- js/script.js
+++ js/script.js
@@ -226,22 +226,29 @@ let equipos = {
 let eContainerPaises = document.querySelector('#accordionPaises')
 
 
-for ( let pais in equipos) {
+for (let pais in equipos) {
 	divContainer = document.createElement('div');
 	divContainer.innerHTML = `
 	<div class="accordion-item">
             <h2 class="accordion-header" id="title-${pais}">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#text-${pais}" aria-expanded="true" aria-controls="${pais}">
+                <button 
+					class="accordion-button" 
+					type="button" 
+					data-bs-toggle="collapse" 
+					data-bs-target="#text-${pais}" 
+					aria-expanded="true" 
+					aria-controls="text-${pais}">
 					${pais}
                 </button>
             </h2>
-            <div id="${pais}" class="accordion-collapse collapse show" aria-labelledby="title-${pais}" data-bs-parent="#accordionPaises">
-                <div class="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <div id="text-${pais}" class="accordion-collapse collapse show" aria-labelledby=title-${pais}" data-bs-parent="#accordionPaises">
+                
+				<div class="accordion-body">
+					
                 </div>
             </div>
         </div>
 	`;
 	eContainerPaises.appendChild(divContainer);
-
 }
+
